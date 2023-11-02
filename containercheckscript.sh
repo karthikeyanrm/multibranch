37f34e1fd1a82134b4316deed4c2469e6cd1d89f
karthikeyanrm <149597952+karthikeyanrm@users.noreply.github.com>
2023-11-02 18:56:39 +0530

    Create containercheckscript.sh

---
+++ containercheckscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if any containers with the name "reactjscontainer" are either exited or running
+containers_to_remove=$(docker ps -q --filter "name=reactjscontainer" --filter "status=exited" --filter "status=running")
+
+if [ -n "$containers_to_remove" ]; then
+  echo "Found exited and/or running containers with the name 'reactjscontainer':"
+  echo "$containers_to_remove"
+
+  # Stop and forcefully remove the containers
+  docker stop $containers_to_remove
+  docker rm -f $containers_to_remove
+
+  echo "Containers with the name 'reactjscontainer' have been removed."
+else
+  echo "No exited or running containers with the name 'reactjscontainer' found. Proceeding to the next step."
+fi
